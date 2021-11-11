c64540e5c8392f209025790b1236ffc70e554dbc
Joe <josephdmasters@gmail.com>
2021-11-11 11:53:20 -0600

    com fix, adding demean and outlier clip

--- pythonSrc/pythonSample.py
+++ pythonSrc/pythonSample.py
@@ -6,16 +6,30 @@ from collections import namedtuple
 import sys
 for x in sys.path:
     print(x)
-sys.path.append('/home/joe/.local/lib/python3.7/dist-packages')
+sys.path.append('.pyenv/versions/3.7.6/lib/python3.7/site-packages/')
 import stomp
+import numpy as np
 from jsmin import jsmin
+
 
 conn = stomp.Connection([("node2.hawkguide.com", 64613)])
 indConfigFile=r"./ind_config.json"
-coefFile=r"./coefs.json"
+parmFile=r"./parms.json"
 
 Trade = namedtuple("Trade", "price size side time")
-Quote = namedtuple("Quote", "bidPrice bidSize askPrice askSize time")
+Quote = namedtuple("Quote", "bidPrice bidSize askPrice askSize com time")
+
+def center_of_mass(ask_vec,bid_vec,mid,decay):
+        print("com")
+        ask_mass = 0
+        bid_mass = 0
+        for q in ask_vec:
+            ask_mass += q[1] * (decay ** (q[0] - mid))
+        for q in bid_vec:
+            bid_mass += q[1] * (decay ** (mid - q[0]))
+        print(np.round(bid_mass - ask_mass, 6))
+        return  np.round(bid_mass - ask_mass, 6)
+
 
 def connect_and_subscribe(conn):
     conn.connect("admin", "Asj874hsaWE38jdsF", wait=True)
@@ -46,39 +60,42 @@ def parseConfigFile(config_file: str):
 
 
 indConfig=parseConfigFile(indConfigFile)
+parms=parseConfigFile(parmFile)
 
 baseIndicatorConfigs=indConfig['baseIndicatorConfigs']
 
 derivedIndicatorConfigs=indConfig['derivedIndicatorConfigs']
 
-coeffs=parseConfigFile(coefFile)['coefs']
+coeffs=parms['coefs']
+means=parms['means']
+outliers=parms['outliers']
 
 class Indicator:
     def __init__(self,indConfig):
-        self.name=indConfig['Name']
+        self.name=indConfig['name']
         self.values=[]
 
     def update(self,trader):
-        if(self.name=="TradePrice"):
+        if(self.name=="trade_price"):
             self.values.append(trader.lastTradePrice)
-        elif(self.name=="TradeSize"):
+        elif(self.name=="trade_size"):
             self.values.append(trader.lastTradeSize)
-        elif(self.name=="TradeSide"):
+        elif(self.name=="trade_side"):
             side=0
             if(trader.lastTradeSide=="b"):
                 side=1
             elif(trader.lastTradeSide=="s"):
                 side=-1
             self.values.append(side)
-        elif (self.name == "BidPrice"):
+        elif (self.name == "bid_price"):
             self.values.append(trader.lastBidPrice)
-        elif (self.name == "BidSize"):
+        elif (self.name == "bid_size"):
             self.values.append(trader.lastBidSize)
-        elif(self.name=="AskPrice"):
+        elif(self.name=="ask_price"):
             self.values.append(trader.lastAskPrice)
-        elif(self.name=="AskSize"):
+        elif(self.name=="ask_size"):
             self.values.append(trader.lastAskSize)
-        elif(self.name=="MidPrice"):
+        elif(self.name=="mid_price"):
             self.values.append((trader.lastBidPrice+trader.lastAskPrice)*0.5)
         else:
             self.values.append(0)
@@ -88,38 +105,59 @@ class DerivedIndicator(Indicator):
     def __init__(self,indConfig):
         super().__init__(indConfig)
         self.config=indConfig
-        self.ind1=indConfig["Ind1"]
-        self.ind2=indConfig.get("Ind2",None)
-        self.combType=self.config['CombType']
+        self.ind1=indConfig["base_ind"]
+        self.ind2=indConfig.get("ind2",None)
+        self.smearFlag=self.config.get("smear",False)
+        self.diffFlag = self.config.get("diff", False)
+        self.combType=self.config.get('combType',None)
+        self.outlierValue=self.config.get('outlierValue',None)
         print("combType=",self.combType)
 
     def update(self,trader):
+        print("update in")
         ind1 = trader.indicators[self.ind1].values
+        val=ind1[-1]
         if(self.ind2 != None):
             ind2 = trader.indicators[self.ind2].values
-        if (self.combType == "Diff"):
-            if (len(ind1) == 1):
-                val=0
-            else:
+        if(self.smearFlag):
+            if((len(ind1)>1) & (ind1[-1]==0)):
+                val=ind1[-2]
+        if(self.diffFlag):
+            if(len(ind1)>1):
                 val=ind1[-1]-ind1[-2]
-        if(self.combType=="Ema"):
+        if(self.combType=="ema"):
             if(len(ind1)==1):
                 val=ind1[-1]
             else:
-                val=ind1[-1]+(1/float(self.config['Alpha']))*ind1[-2]
-        if(self.combType=="Eq"):
+                val=ind1[-1]+(1/float(self.config['alpha']))*ind1[-2]
+        if(self.combType=="eq"):
             if(abs(ind1[-1]-ind2[-1])<EPS):
                 val=1
             else:
                 val=0
-        if(self.combType=="Sum"):
+        if(self.combType=="sum"):
             val=ind1[-1]+ind2[-1]
-        if(self.combType=="Sub"):
+        if(self.combType=="minus"):
             val=ind1[-1]-ind2[-1]
-        if(self.combType=="Prod"):
+        if(self.combType=="prod"):
             val=ind1[-1]*ind2[-1]
+
+        outlier_val = outliers.get(self.name)
+        if self.outlierValue != None:
+            outlier_val = min(outlier_val,self.outlier_value)
+        if(outlier_val!=None):
+            if indConfig.get("outlier_reject"):
+                vsl = int((abs(val) <= outlier_val)) * val
+            elif indConfig.get("outlier_clip"):
+                val = np.clip(val,-outlier_val, outlier_val)
+
+        if(indConfig.get("demean")):
+            if(means.get(self.name)!=None):
+                val=val-means[self.name]
+
         print(self.name," derived value=",val)
         self.values.append(val)
+        print("out")
 
 class Trader:
     def __init__(self, initialValue):
@@ -134,10 +172,10 @@ class Trader:
         self.coeffs=coeffs
         self.indicators={}
         for config in self.baseIndicatorConfigs:
-            self.indicators[config['Name']]=Indicator(config)
+            self.indicators[config['name']]=Indicator(config)
         self.derivedIndicators={}
         for config in self.derivedIndicatorConfigs:
-            self.indicators[config['Name']]=DerivedIndicator(config)
+            self.indicators[config['name']]=DerivedIndicator(config)
         self.preds=[]
         self.lastTradePrice=0
         self.lastTradeSize=0
@@ -146,6 +184,7 @@ class Trader:
         self.lastBidSize=0
         self.lastAskPrice=0
         self.lastAskSize=0
+        self.last_com={}
         self.tradeFlag=0
         self.lastPred=0
 
@@ -215,7 +254,7 @@ class Trader:
         self.lastBidSize=quote.bidSize
         self.lastAskPrice=quote.askPrice
         self.lastAskSize=quote.askSize
-        print("receive ob")
+        self.lastCom=quote.com
         return []
 
     def receiveCandle(self, candle, positions, orders, options):
@@ -231,7 +270,6 @@ traderInstance = Trader("{}")
 
 def updateIndicators():
     global traderInstance
-    print("updating")
     for ind in traderInstance.indicators.keys():
         traderInstance.indicators[ind].update(traderInstance)
     traderInstance.postUpdate()
@@ -295,7 +333,7 @@ class MyListener(stomp.ConnectionListener):
         #print("received a message", data)
         #print("\n")
         if (data['e'] == 'tf'):
-            print(data)
+            print("tf",data)
             tradeMsg=data['d']
             trade=Trade(price=tradeMsg['r'], size=tradeMsg['a'], side=tradeMsg['s'],time=tradeMsg['ts'])
             traderInstance.receiveTrade(trade)
@@ -304,27 +342,34 @@ class MyListener(stomp.ConnectionListener):
 
         if (data['e'] == 'obstream'):
             quoteMsg=data['d']
-            bidVec=quoteMsg['b']
-            while ((len(bidVec)>0) and (bidVec[-1][1]==0)):
-                bidVec=bidVec[:-1]
-            if(len(bidVec)==0):
+            bid_vec=quoteMsg['b']
+            while ((len(bid_vec)>0) and (bid_vec[-1][1]==0)):
+                bid_vec=bid_vec[:-1]
+            if(len(bid_vec)==0):
                 bidPrice = 0
                 bidSize = 0
             else:
-                bidPrice = bidVec[-1][0]
-                bidSize = bidVec[-1][1]
+                bidPrice = bid_vec[-1][0]
+                bidSize = bid_vec[-1][1]
 
-            askVec=quoteMsg['a']
-            while ((len(askVec)>0) and (askVec[0][1]==0)):
-                askVec=askVec[1:]
-            if(len(askVec)==0):
+
+            ask_vec=quoteMsg['a']
+            while ((len(ask_vec)>0) and (ask_vec[0][1]==0)):
+                ask_vec=ask_vec[1:]
+            if(len(ask_vec)==0):
                 askPrice=0
                 askSize=0
             else:
-                askPrice=askVec[0][0]
-                askSize=askVec[0][1]
+                askPrice=ask_vec[0][0]
+                askSize=ask_vec[0][1]
+            print("new ask vec=",ask_vec)
+            com={}
+            for d in indConfig.get("decays"):
+                com[d]=0
+                if((len(ask_vec)!=0)&(len(bid_vec)!=0)):
+                    com[d]=center_of_mass(ask_vec,bid_vec,0.5*(askPrice+bidPrice),d)
 
-            quote=Quote(bidPrice=bidPrice, bidSize=bidSize,askPrice=askPrice, askSize=askSize,time=quoteMsg['ts'])
+            quote=Quote(bidPrice=bidPrice, bidSize=bidSize,askPrice=askPrice, askSize=askSize,com=com,time=quoteMsg['ts'])
             traderInstance.receiveOb(quote)
 
 
@@ -334,6 +379,7 @@ class MyListener(stomp.ConnectionListener):
 
 
 conn.set_listener("", MyListener(conn))
+print("listening")
 connect_and_subscribe(conn)
 
 time.sleep(1009999)
